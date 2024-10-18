ca317682a0cb8957e4f017abdee76648880014b1
fthimus <fthimus@gmail.com>
2024-10-18 07:44:12 +0700

    Init commit

---
+++ GithubUserActivityCLI/GithubUserActivityCLI/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            ShowUsage();
+            return;
+        }
+        Console.WriteLine($"Hello {args[0]}");
+    }
+
+    static void ShowUsage()
+    {
+        Console.WriteLine("Show Usage");
+    }
+}
